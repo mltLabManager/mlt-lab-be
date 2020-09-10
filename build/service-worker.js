b03324b57825895d41a673c404fce7e7d0e2282d
Sapir-sh <sapir2167@gmail.com>
2020-09-10 12:32:39 +0300

    upload client - fix filter

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e93d8c0a372eea53e535184f77a8891.js"
+  "/precache-manifest.991d89dc7f479a222719ba1aa0fdedce.js"
 );
 
 self.addEventListener('message', (event) => {
