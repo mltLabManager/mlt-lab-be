80cea9968478b8e5d5282688afc19b1168662775
Sapir-sh <sapir2167@gmail.com>
2020-09-10 12:37:00 +0300

    update client - fix routing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.991d89dc7f479a222719ba1aa0fdedce.js"
+  "/precache-manifest.f5d8911781ddd4e6c11e5b029a96dc7a.js"
 );
 
 self.addEventListener('message', (event) => {
